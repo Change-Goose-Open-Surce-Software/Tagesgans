ae35551a3ae4d3417c59b0802c29985ba4ae3e28
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-02-14 17:22:41 +0100

    Set up installation script and dependencies
    Added directories for tagesgans and icons, downloaded the Install.sh script, and installed necessary packages.

--- Install.sh
+++ Install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-#Installscript
+mkdir -p ~/.local/bin/tagesgans
+mkdir -p ~/.local/share/icons/Goose
+
+wget -O https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Install.sh
+
+sudo apt install python3 python3-pyqt5 python3-pyqt5.qtmultimedia python3-pyqt5.qtmultimediawidgets qgis gnome-contacts gnome-calendar xdg-utils gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav -y
