5013bb8c002d470252e0cdf6df8ab1e40bdc5a43
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-02-14 17:46:33 +0100

    Add desktop entry installation and update permissions
    Added installation of desktop entry for Tagesgans and updated permissions.

--- Install.sh
+++ Install.sh
@@ -9,9 +9,13 @@ wget -O ~/.local/bin/tagesgans/editor.py https://raw.githubusercontent.com/Chang
 wget -O ~/.local/share/icons/Goose/tagesgans.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/icon.py
 wget -O ~/.local/share/icons/Goose/time.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Time.png
 wget -O ~/.local/share/icons/Goose/label.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Label.png
+wget -O ~/.local/share/Applications/tagesgans.desktop https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Tagesgans.desktop
 
 chmod +x ~/.local/bin/reader.py
 chmod +x ~/.local/bin/editor.py
 chmod +x ~/.local/bin/tagesgans.py
+chmod +x ~/.local/share/Applications/tagesgans.desktop
 
+sudo apt update
 sudo apt install python3 python3-pyqt5 python3-pyqt5.qtmultimedia python3-pyqt5.qtmultimediawidgets qgis gnome-contacts gnome-calendar xdg-utils gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav -y
+sudo update-desktop-database
