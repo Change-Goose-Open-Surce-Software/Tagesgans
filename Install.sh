c7db30bd19b45ae3edc2ccfe75427a8b9e5e657e
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-02-14 17:26:01 +0100

    Download reader.py to local bin directory

--- Install.sh
+++ Install.sh
@@ -3,6 +3,6 @@
 mkdir -p ~/.local/bin/tagesgans
 mkdir -p ~/.local/share/icons/Goose
 
-wget -O https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Install.sh
+wget -O ~/.local/bin/tagesgans/reader.py https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/reader.py
 
 sudo apt install python3 python3-pyqt5 python3-pyqt5.qtmultimedia python3-pyqt5.qtmultimediawidgets qgis gnome-contacts gnome-calendar xdg-utils gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav -y
