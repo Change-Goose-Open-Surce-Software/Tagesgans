0bd3e2642de0ef796b54175864b7a93cb0a99522
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-02-14 17:30:29 +0100

    Download additional scripts and set executable permissions
    Added commands to download and set permissions for additional Python scripts and an icon.

--- Install.sh
+++ Install.sh
@@ -4,5 +4,12 @@ mkdir -p ~/.local/bin/tagesgans
 mkdir -p ~/.local/share/icons/Goose
 
 wget -O ~/.local/bin/tagesgans/reader.py https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/reader.py
+wget -O ~/.local/bin/tagesgans/tagesgans.py https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/tagesgans.py
+wget -O ~/.local/bin/tagesgans/editor.py https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/editor.py
+wget -O ~/.local/bin/tagesgans/tagesgans.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/icon.py
+
+chmod +x ~/.local/bin/reader.py
+chmod +x ~/.local/bin/editor.py
+chmod +x ~/.local/bin/tagesgans.py
 
 sudo apt install python3 python3-pyqt5 python3-pyqt5.qtmultimedia python3-pyqt5.qtmultimediawidgets qgis gnome-contacts gnome-calendar xdg-utils gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav -y
