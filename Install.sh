ff7e055ff6dd01c8dfd5f81f637a674b6bf21b8b
Change-Goose-Open-Surce-Software <dimitri.andreas.hiebert@gmail.com>
2026-02-14 18:17:04 +0100

    Fix file paths for icons and desktop entry

--- Install.sh
+++ Install.sh
@@ -6,15 +6,15 @@ mkdir -p ~/.local/share/icons/Goose
 wget -O ~/.local/bin/tagesgans/reader.py https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/reader.py
 wget -O ~/.local/bin/tagesgans/tagesgans.py https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/tagesgans.py
 wget -O ~/.local/bin/tagesgans/editor.py https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/editor.py
-wget -O ~/.local/share/icons/Goose/tagesgans.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/icon.py
+wget -O ~/.local/share/icons/Goose/tagesgans.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/icon.png
 wget -O ~/.local/share/icons/Goose/time.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Time.png
 wget -O ~/.local/share/icons/Goose/label.png https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Label.png
-wget -O ~/.local/share/Applications/tagesgans.desktop https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Tagesgans.desktop
+wget -O ~/.local/share/applications/tagesgans.desktop https://raw.githubusercontent.com/Change-Goose-Open-Surce-Software/Tagesgans/main/Tagesgans.desktop
 
 chmod +x ~/.local/bin/tagesgans/reader.py
 chmod +x ~/.local/bin/tagesgans/editor.py
 chmod +x ~/.local/bin/tagesgans/tagesgans.py
-chmod +x ~/.local/share/Applications/tagesgans.desktop
+chmod +x ~/.local/share/applications/tagesgans.desktop
 
 sudo apt update
 sudo apt install python3 python3-pyqt5 python3-pyqt5.qtmultimedia qgis gnome-contacts gnome-calendar xdg-utils gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav -y
